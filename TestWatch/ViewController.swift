c2d0a500ae43f3c6f6dccfb417c8610258049d3e
eric360 <eric-richard.hongtuanha@360learning.com>
2015-09-22 17:53:05 +0200

    Initial Commit

---
+++ TestWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestWatch
+//
+//  Created by eric_360 on 22/09/15.
+//  Copyright © 2015 Eric Hong tuan ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
